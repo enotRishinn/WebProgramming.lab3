b8211c26bdeb5426adfddf51d19ee95e2546ea9d
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-04-18 15:52:16 +0300

    4 commit

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function createQuoteHTML() {
 
-  drawingImage(canvas) {
-    context = canvas.getContext("2d");
-  }
-
   var canvas = document.createElement("canvas");
-  canvas.style.width = "80%";
-  canvas.style.height = "80%";
+  canvas.style.width = "500px";
+  canvas.style.height = "500px";
   document.body.appendChild(canvas);
-  drawingImage(canvas);
+
 
   var quoteButton = document.createElement("button");
   quoteButton.textContent = "Сгенерировать новый коллаж с цитатой";
   quoteButton.style.padding = "1%";
+  quoteButton.style.margin = "2%";
   quoteButton.onclick = function() {
 
   }
@@ -20,12 +17,17 @@ function createQuoteHTML() {
 
   var saveButton = document.createElement("button");
   saveButton.textContent = "Сохранить коллаж";
-  saveButton.style.marginLeft = "3%";
+  quoteButton.style.margin = "2%"
   saveButton.style.padding = "1%";
   saveButton.onclick = function() {
 
   }
   document.body.appendChild(saveButton);
+}
+
+function drawingImage(canvas) {
+  context = canvas.getContext("2d");
+  var img = new Image();
 }
 
 createQuoteHTML();
