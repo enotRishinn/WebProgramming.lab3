15581c263f8e3f6cf9db940b04063defe187b0bf
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-04-18 15:02:17 +0300

    2 commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function createQuoteHTML() {
-  
+
   var canvas = document.createElement("canvas");
   canvas.style.width = "80%";
   canvas.style.height = "80%";
@@ -7,6 +7,7 @@ function createQuoteHTML() {
 
   var quoteButton = document.createElement("button");
   quoteButton.textContent = "Сгенерировать новый коллаж с цитатой";
+  quoteButton.style.padding = "1%";
   quoteButton.onclick = function() {
 
   }
@@ -15,6 +16,7 @@ function createQuoteHTML() {
   var saveButton = document.createElement("button");
   saveButton.textContent = "Сохранить коллаж";
   saveButton.style.marginLeft = "3%";
+  saveButton.style.padding = "1%";
   saveButton.onclick = function() {
 
   }
