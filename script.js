e58aae5b858eaa67cac22a05a4f70a45fa0dc951
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-04-19 07:46:34 +0300

    6 commit

--- script.js
+++ script.js
@@ -8,19 +8,11 @@ function createQuoteHTML() {
   drawingImage(canvas);
 
 
-  var quoteButton = document.createElement("button");
-  quoteButton.textContent = "Сгенерировать новый коллаж с цитатой";
-  quoteButton.style.padding = "1%";
-  quoteButton.style.margin = "2%";
-  quoteButton.onclick = function() {
-
-  }
-  document.body.appendChild(quoteButton);
-
   var saveButton = document.createElement("button");
   saveButton.textContent = "Сохранить коллаж";
   saveButton.style.margin = "2%"
   saveButton.style.padding = "1%";
+  saveButton.style.display = "flex";
   saveButton.onclick = function() {
 
   }
@@ -30,18 +22,23 @@ function createQuoteHTML() {
 function drawingImage(canvas) {
 
   var context = canvas.getContext("2d");
-  var collections = [762960, 162326, 1538121, 162468, 357786, 463870];
+  var collections = [762960, 1538121, 162468, 357786, 1346770, 1075856, 162232];
+  var index;
+  var num;
+
   for (var i = 0; i < 2; i++) {
     for (var j = 0; j < 2; j++) {
+
+      num = collections[Math.floor(Math.random() * collections.length)];
+      index = collections.indexOf(num);
+      collections.splice(index, 1);
+
       var img = new Image();
-      num = collections[Math.floor(Math.random() * collections.length)]
         img.onload = (function(i, j, img) {
             return function() {
             context.drawImage(img, i*300, j*200);
-            num = collections[Math.floor(Math.random() * collections.length)];
               }
           })(i, j, img);
-
           img.src = "https://source.unsplash.com/collection/"+ num + "/300x200";
       }
   }
